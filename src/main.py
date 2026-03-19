a50c3576f01428e232e76b3b4c12d4848813b448
MorenoMarrara <moreno.marrara@gmail.com>
2026-03-20 00:14:21 +0100

    restructuring

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+from platforms.Qiskit import get_qrc_features
+
+# --- BEISPIEL ANWENDUNG ---
+
+# Beispiel-Daten (X: Zeitreihen-Fenster, y: Zielwert)
+X_raw = [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4]]
+y_train = [0.5, 0.6, 0.7]
+
+# Features durch das Quanten-Reservoir generieren
+X_quantum = np.array([get_qrc_features(seq,3) for seq in X_raw])
+
+# Klassische Lineare Regression auf den Quanten-Features
+model = LinearRegression()
+model.fit(X_quantum, y_train)
+
+print("Quanten-Features Shape:", X_quantum.shape)
+print("Vorhersage für neue Daten:", model.predict(X_quantum[:1]))
--- src/platforms/Qiskit/impl.py
+++ src/platforms/Qiskit/impl.py
@@ -1,8 +1,6 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
-
 
 def get_qrc_features(input_sequence, n_qubits=4):
     """
@@ -37,19 +35,3 @@ def get_qrc_features(input_sequence, n_qubits=4):
 
     return features / 1024  # Normalisierung auf [0, 1]
 
-
-# --- BEISPIEL ANWENDUNG ---
-
-# Beispiel-Daten (X: Zeitreihen-Fenster, y: Zielwert)
-X_raw = [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4]]
-y_train = [0.5, 0.6, 0.7]
-
-# Features durch das Quanten-Reservoir generieren
-X_quantum = np.array([get_qrc_features(seq,3) for seq in X_raw])
-
-# Klassische Lineare Regression auf den Quanten-Features
-model = LinearRegression()
-model.fit(X_quantum, y_train)
-
-print("Quanten-Features Shape:", X_quantum.shape)
-print("Vorhersage für neue Daten:", model.predict(X_quantum[:1]))
