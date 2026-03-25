0ce4cda2843b2d027c286264a438b07b4a35d8ce
MorenoMarrara <moreno.marrara@gmail.com>
2026-03-25 10:00:03 +0100

    test main.py for loading data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from pandas import read_csv
 
 from platforms.my_qiskit import get_qrc_features
 from data.loading.utils import load_data_from
@@ -12,8 +13,10 @@ from data.loading.geosphere import Geosphere
 # X_raw = [[0.1, 0.2], [0.2, 0.3], [0.3, 0.4]]
 # y_train = [0.5, 0.6, 0.7]
 
-data = load_data_from(Geosphere())
-X_raw, y_train = data[1], data[2]
+# data = load_data_from(Geosphere())
+
+data = Geosphere().load_data_into_memory()
+X_raw, y_train = data["dd", "ddx"], data["pred"]
 
 # Features durch das Quanten-Reservoir generieren
 X_quantum = np.array([get_qrc_features(seq,2) for seq in X_raw])
