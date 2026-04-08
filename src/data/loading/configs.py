89a55985930f67e84ee51d97b74efb8aad6b4449
MorenoMarrara <moreno.marrara@gmail.com>
2026-04-08 22:49:07 +0200

    minor adaptations

--- src/data/loading/configs.py
+++ src/data/loading/configs.py
@@ -1,4 +1,6 @@
 from pathlib import Path
 
 DEV_DATA_DIR = Path("/home/moreno/Documents/UNI/FHCampus/SoftwareDesignAndEngineering/Education/Courses/3/DL/Project/ml_pipeline/data_acquisition/Geosphere/data/measurements/107")
-MODEL_DIR_CLASSICAL = Path("/home/moreno/Documents/UNI/FHCampus/SoftwareDesignAndEngineering/Education/Thesis/Project/models/classical")
+MODEL_DIR = Path("/home/moreno/Documents/UNI/FHCampus/SoftwareDesignAndEngineering/Education/Thesis/Project/models")
+MODEL_DIR_CLASSICAL = MODEL_DIR / "classical"
+MODEL_DIR_QRC = MODEL_DIR / "QRC"
