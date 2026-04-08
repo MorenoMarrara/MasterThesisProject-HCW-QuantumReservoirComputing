6475820191ea112f2469f8072ec7e93cc97e1b46
MorenoMarrara <moreno.marrara@gmail.com>
2026-04-08 15:36:25 +0200

    improved classical time-series analysis:  - almost done:   . need assessment/evaluation of results   . need visualization of results

--- src/data/loading/configs.py
+++ src/data/loading/configs.py
@@ -1,3 +1,4 @@
 from pathlib import Path
 
 DEV_DATA_DIR = Path("/home/moreno/Documents/UNI/FHCampus/SoftwareDesignAndEngineering/Education/Courses/3/DL/Project/ml_pipeline/data_acquisition/Geosphere/data/measurements/107")
+MODEL_DIR_CLASSICAL = Path("/home/moreno/Documents/UNI/FHCampus/SoftwareDesignAndEngineering/Education/Thesis/Project/models/classical")
